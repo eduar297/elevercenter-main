59f575efe334c6950ceefaa3be9d0b39efdfb7b4
Eduar2 <eduar2.moreira@gmail.com>
2021-06-01 17:38:06 -0400

    first commit

---
+++ build/precache-manifest.5038ca015724a7c324537e7f4d1b722b.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78fa1987a4b0b7b611e347c538efef42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a38ef21f0cbf7bac427",
+    "url": "/static/css/main.f5b5f9f6.chunk.css"
+  },
+  {
+    "revision": "4684e06b4aa3a75a3d86",
+    "url": "/static/js/2.a11c69e5.chunk.js"
+  },
+  {
+    "revision": "1a38ef21f0cbf7bac427",
+    "url": "/static/js/main.38b14a25.chunk.js"
+  },
+  {
+    "revision": "61336d579c171b4ac441",
+    "url": "/static/js/runtime-main.3816e7e6.js"
+  },
+  {
+    "revision": "b92da187846bfbbcac0ff58dc36e25d7",
+    "url": "/static/media/avatar info.b92da187.PNG"
+  },
+  {
+    "revision": "0918e9268f02cf4954c4f65b39e0b85f",
+    "url": "/static/media/avatar1.0918e926.PNG"
+  },
+  {
+    "revision": "3c52acc4c01e57ae837b97016feb4320",
+    "url": "/static/media/baby.3c52acc4.svg"
+  },
+  {
+    "revision": "5a744564db91306a17ebaf3c5b8e7ea9",
+    "url": "/static/media/bell.5a744564.svg"
+  },
+  {
+    "revision": "e573723676c398e2b18c3f5c8555a870",
+    "url": "/static/media/benefits.e5737236.png"
+  },
+  {
+    "revision": "7b7eaedf059741ecb353738614340571",
+    "url": "/static/media/car.7b7eaedf.svg"
+  },
+  {
+    "revision": "dabba708bfa9782aa374c0698112c81e",
+    "url": "/static/media/cat3.dabba708.png"
+  },
+  {
+    "revision": "fd14ea04d37eb10eca9bb8e518c0219f",
+    "url": "/static/media/coffe.fd14ea04.svg"
+  },
+  {
+    "revision": "c16acea2e3bc1163917e000c8adfc118",
+    "url": "/static/media/comer.c16acea2.svg"
+  },
+  {
+    "revision": "07546313b3b1fc5ccea77dd60da8da03",
+    "url": "/static/media/contact.07546313.png"
+  },
+  {
+    "revision": "2d6cd6ed1342f66a9e64ad4a340ccc58",
+    "url": "/static/media/contacts.2d6cd6ed.png"
+  },
+  {
+    "revision": "3bce097a747dd5701b6bc6f932b5f830",
+    "url": "/static/media/desk.3bce097a.svg"
+  },
+  {
+    "revision": "dc22813a53015f839371ac78166f2348",
+    "url": "/static/media/event1.dc22813a.jpg"
+  },
+  {
+    "revision": "b771f69e02708a3754d616b216697b85",
+    "url": "/static/media/event2.b771f69e.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "eebad98a1c8d33777d64fef9e1ad5d18",
+    "url": "/static/media/footer.eebad98a.png"
+  },
+  {
+    "revision": "ae39bb15a393434a32b170123dd4a9df",
+    "url": "/static/media/hero-bg.ae39bb15.jpg"
+  },
+  {
+    "revision": "3cb6b22d9ad8973c62cf02fa4f66c30a",
+    "url": "/static/media/logo.3cb6b22d.svg"
+  },
+  {
+    "revision": "98aaec2aad03a43dd90ff52746a13134",
+    "url": "/static/media/mask.98aaec2a.svg"
+  },
+  {
+    "revision": "1e9aafe74253e19c96f75e5077263e40",
+    "url": "/static/media/not1.1e9aafe7.png"
+  },
+  {
+    "revision": "ccaa0038cdcb6b8c1a9eecddc5b7d584",
+    "url": "/static/media/not2.ccaa0038.png"
+  },
+  {
+    "revision": "d2d4457d3193fc6916486c5b1c222b97",
+    "url": "/static/media/not3.d2d4457d.png"
+  },
+  {
+    "revision": "b8c412936021cb97547d0362bd5ff6d7",
+    "url": "/static/media/not4.b8c41293.png"
+  },
+  {
+    "revision": "f84aa29b09cb396c9e167cabc3544506",
+    "url": "/static/media/people.f84aa29b.svg"
+  },
+  {
+    "revision": "572671b30b6e4ab61de6b0509740c1cf",
+    "url": "/static/media/secure.572671b3.svg"
+  },
+  {
+    "revision": "90afd7474eda7713ae85a86974b9d7f4",
+    "url": "/static/media/sofa.90afd747.svg"
+  },
+  {
+    "revision": "bc589f4c92bc580bd6187e80678d51d1",
+    "url": "/static/media/texh.bc589f4c.svg"
+  },
+  {
+    "revision": "53d9a2fbc128297fd8e07940ca1fc3bd",
+    "url": "/static/media/wifi.53d9a2fb.svg"
+  }
+]);
